9e27cb3d20bb3b52476b22663ac9c865d1be9540
Leonardo M Carvalho <104258969+mercesleonardo@users.noreply.github.com>
2024-02-15 18:21:18 -0300

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -46,7 +46,7 @@
                 <i class="fas fa-bars"></i>
             </button>
             <form action="<?= $BASE_URL ?>search.php" method="GET" id="search-form" class="form-inline my-2 my-lg-0">
-                <input type="text" name="q" id="search" class="form-control mr-sm-2" type="search" placeholder="Buscar Filmes" aria-label="Search">
+                <input type="text" name="q" id="search" class="form-control mr-sm-2" type="search" placeholder="Buscar Jogos" aria-label="Search">
                 <button class="btn my-2 my-sm-0" type="submit">
                     <i class="fas fa-search"></i>
                 </button>
